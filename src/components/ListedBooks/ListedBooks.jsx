a0ec425f9601261bf90d9b1f41ce52ada2c32a11
NazmusSakib <n.sakib.sad@gmail.com>
2024-11-19 15:11:23 +0600

    First commit

---
+++ src/components/ListedBooks/ListedBooks.jsx
@@ -0,0 +1,11 @@
+
+const ListedBooks = () => {
+  return (
+    <div>
+      <h1>This is listed Books
+      </h1>
+    </div>
+  );
+};
+
+export default ListedBooks;
---
+++ src/main.jsx
@@ -0,0 +1,38 @@
+import * as React from "react";
+import * as ReactDOM from "react-dom/client";
+import {
+  createBrowserRouter,
+  RouterProvider,
+} from "react-router-dom";
+import "./index.css";
+import Root from "./components/Root/Root";
+import Home from "./components/Home/Home";
+import ListedBooks from "./components/ListedBooks/ListedBooks";
+import PagesToRead from "./components/PagesToRead/PagesToRead";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Root></Root>,
+    children: [
+      {
+        path: "/",
+        element: <Home></Home>
+      },
+      {
+        path: "/listedBooks",
+        element: <ListedBooks></ListedBooks>
+      },
+      {
+        path: "/pagesToRead",
+        element: <PagesToRead></PagesToRead>
+      }
+    ]
+  },
+]);
+
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <React.StrictMode>
+    <RouterProvider router={router} />
+  </React.StrictMode>
+);
