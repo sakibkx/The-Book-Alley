ed0ec77696de5ef08829964a64d04ad510d145ac
NazmusSakib <n.sakib.sad@gmail.com>
2024-11-20 03:08:43 +0600

    json file completed

--- src/components/Books/Books.jsx
+++ src/components/Books/Books.jsx
@@ -1,8 +1,24 @@
+import { useEffect, useState } from "react";
+import Book from "../Book/Book";
 
 const Books = () => {
+  
+  const [books, setBooks] = useState([]);
+
+  useEffect( () => {
+    fetch('../../../public/Books.json')
+    .then(res => res.json())
+    .then(data => setBooks(data))
+  }, [])
+
   return (
     <div>
       <h1 className="text-5xl font-bold font-serif text-center mb-12">Books</h1>
+      <div>
+        {
+          books.map( book => <Book key={book.bookId} book={book}></Book>)
+        }
+      </div>
     </div>
   );
 };
