b43b6785afc90df41f74e0a1d0000ab564fb5d6e
NazmusSakib <n.sakib.sad@gmail.com>
2024-11-20 13:10:32 +0600

    cards are designed

--- src/components/Book/Book.jsx
+++ src/components/Book/Book.jsx
@@ -1,21 +1,27 @@
+import { MdOutlineStarBorder } from "react-icons/md";
 
 const Book = (book) => {
   return (
-    <div className="card bg-base-100 w-96 shadow-xl">
-      <figure>
+    <div className="card w-96 shadow-xl border border-black p-6">
+      <figure className="bg-zinc-200 px-24 py-8">
         <img
-          src="https://img.daisyui.com/images/stock/photo-1606107557195-0e29a4b5b4aa.webp"
-          alt="Shoes" />
+          src= {book.book.image}
+          alt="Shoes" className="h-44" />
       </figure>
-      <div className="card-body">
-        <h2 className="card-title">
-          Shoes!
-          <div className="badge badge-secondary">NEW</div>
-        </h2>
-        <p>If a dog chews shoes whose shoes does he choose?</p>
-        <div className="card-actions justify-end">
-          <div className="badge badge-outline">Fashion</div>
-          <div className="badge badge-outline">Products</div>
+      <div className="card-body p-0 gap-0">
+        <div className="mt-6 mb-4">
+          {
+            book.book.tags.map( (tag, index) => <span key={index} className="bg-green-50 text-base text-success font-medium py-2 px-4 rounded-3xl me-3">{tag}</span> )
+          }
+        </div>
+        <h2 className="text-2xl font-bold font-serif mb-4">{book.book.bookName}</h2>
+        <h4 className="text-base font-medium pb-5 border-b border-dashed mb-5">By : {book.book.author}</h4>
+        <div className="text-base font-medium flex justify-between">
+          <h4>{book.book.category}</h4>
+          <div className="flex gap-x-2 items-center">
+            <span>{book.book.rating}</span>
+            <MdOutlineStarBorder className="text-lg"/>
+          </div>
         </div>
       </div>
     </div>
--- src/components/Books/Books.jsx
+++ src/components/Books/Books.jsx
@@ -14,7 +14,7 @@ const Books = () => {
   return (
     <div>
       <h1 className="text-5xl font-bold font-serif text-center mb-12">Books</h1>
-      <div>
+      <div className="grid grid-cols-3 gap-6 mb-32">
         {
           books.map( book => <Book key={book.bookId} book={book}></Book>)
         }
